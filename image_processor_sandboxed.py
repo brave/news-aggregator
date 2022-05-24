3f897abbddda3bd146259e9f8915920741d305c1
MCMXC <16797721+mcmxcdev@users.noreply.github.com>
2022-05-24 18:49:33 +0200

    chore: fix reported pylint issues
    - fix/disable reported pylint issues
    - add .vscode folder to .gitignore

--- image_processor_sandboxed.py
+++ image_processor_sandboxed.py
@@ -20,11 +20,13 @@ s3_resource = boto3.resource('s3')
 
 wasm_path = 'wasm_thumbnail.wasm'
 wasm_store = Store(engine.JIT(Compiler))
+# pylint: disable=consider-using-with
 wasm_module = Module(wasm_store, open(wasm_path, 'rb').read())
 
 
 def resize_and_pad_image(image_bytes, width, height, size, cache_path):
-    pathlib.Path(os.path.dirname(cache_path)).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(os.path.dirname(cache_path)).mkdir(
+        parents=True, exist_ok=True)
     pid = os.fork()
     if pid == 0:
         instance = Instance(wasm_module)
@@ -35,17 +37,18 @@ def resize_and_pad_image(image_bytes, width, height, size, cache_path):
         memory[0:image_length] = image_bytes
 
         try:
-            output_pointer = instance.exports.resize_and_pad(input_pointer, image_length, width, height, size)
+            output_pointer = instance.exports.resize_and_pad(
+                input_pointer, image_length, width, height, size)
         except RuntimeError:
             logging.warning("resize_and_pad() hit a RuntimeError (length=%s, width=%s, height=%s, size=%s): %s.failed",
                             image_length, width, height, size, cache_path)
-            with open("%s.failed" % (cache_path), 'wb+') as out_image:
+            with open(f'{cache_path}.failed', 'wb+', encoding="utf-8") as out_image:
                 out_image.write(image_bytes)
             sys.exit(1)
 
         memory = instance.exports.memory.uint8_view(output_pointer)
         out_bytes = bytes(memory[:size])
-        with open("%s.pad" % (cache_path), 'wb+') as out_image:
+        with open(f'{cache_path}.pad', 'wb+', encoding="utf-8") as out_image:
             out_image.write(out_bytes)
         sys.exit(0)
 
@@ -76,8 +79,8 @@ class ImageProcessor():
         self.s3_bucket = s3_bucket
 
     def cache_image(self, url):
-        cache_fn = "%s.jpg" % (hashlib.sha256(url.encode('utf-8')).hexdigest())
-        cache_path = "./feed/cache/%s" % (cache_fn)
+        cache_fn = f'{hashlib.sha256(url.encode("utf-8")).hexdigest()}.jpg'
+        cache_path = f'./feed/cache/{cache_fn}'
 
         # if we have it dont do it again
         if os.path.isfile(cache_path):
@@ -86,9 +89,10 @@ class ImageProcessor():
         if not config.NO_UPLOAD:
             exists = False
             try:
-                s3_resource.Object(self.s3_bucket, "brave-today/cache/%s.pad" % (cache_fn)).load()
+                s3_resource.Object(
+                    self.s3_bucket, f'brave-today/cache/{cache_fn}.pad').load()
                 exists = True
-            except ValueError as e:
+            except ValueError:
                 exists = False  # make tests work
             except botocore.exceptions.ClientError as e:
                 if e.response['Error']['Code'] == "404":
@@ -106,7 +110,8 @@ class ImageProcessor():
             return None  # skipping (image exceeds maximum size)
         except requests.exceptions.HTTPError as e:
             if e.response.status_code not in (403, 429, 500, 502, 503):
-                logging.error("Failed to get image [%s]: %s", e.response.status_code, url)
+                logging.error(
+                    "Failed to get image [%s]: %s", e.response.status_code, url)
             return None
 
         if not resize_and_pad_image(content, 1168, 657, 250000, cache_path):
@@ -114,5 +119,6 @@ class ImageProcessor():
             return None
 
         if self.s3_bucket and not config.NO_UPLOAD:
-            upload_file("feed/cache/%s.pad" % (cache_fn), self.s3_bucket, "brave-today/cache/%s.pad" % (cache_fn))
+            upload_file(f'feed/cache/{cache_fn}.pad',
+                        self.s3_bucket, f'brave-today/cache/{cache_fn}.pad')
         return cache_fn
