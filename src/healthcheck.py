ab18b3571ee40c41c63eb03138e857c4be312f31
Nicholas Porter <nporter@brave.com>
2023-10-03 22:55:10 -0700

    PR feedback
    - import config variables
    - Set expired date only if 80% of files are older than 3 hours
    - Use sources_dir config
    - Use s3_config from utils

--- src/healthcheck.py
+++ src/healthcheck.py
@@ -1,61 +1,57 @@
 import os
 import json
-import boto3
 
-from utils import upload_file
+from config import get_config
+from utils import upload_file, s3_client
 from datetime import datetime, timedelta
 
-def main():	
-	boto_session = boto3.Session()
-	s3_client = boto_session.client("s3")
-	s3_resource = boto3.resource("s3")
+def main():    
+    config = get_config()
+    bucket_name = config.pub_s3_bucket
+    sources_dir = config.sources_dir
 
-	# Initialize S3 client
-	s3_client = boto_session.client("s3")
-	bucket_name = ''
+    # Initialize an empty dict to store the results
+    file_modification_dates = {}
 
-	sources_dir = "../sources"
+    s3_file_keys = []
+    # Update the s3_file_keys array by iterating through the sources dir
+    for filename in os.listdir(sources_dir):
+        if filename.startswith("sources.") and filename.endswith(".csv"):
+            s3_file_keys.append(os.path.join(sources_dir, filename))
 
-	# Initialize an empty dict to store the results
-	file_modification_dates = {}
+    # Iterate through the list of file keys and retrieve the last modified date
+    for file_key in s3_file_keys:
+        try:
+            response = s3_client.head_object(Bucket=bucket_name, Key=file_key)
+            last_modified = response['LastModified']
+            file_modification_dates[file_key] = last_modified
+        except Exception as e:
+            print(f"Error retrieving last modified date for {file_key}: {e}")
 
-	s3_file_keys = []
-	# Update the s3_file_keys array by iterating through the directory
-	for filename in os.listdir(sources_dir):
-		if filename.startswith("sources.") and filename.endswith(".csv"):
-			s3_file_keys.append(os.path.join(sources_dir, filename))
+    # Check if any files are older than 3 hours
+    current_time = datetime.now()
+    expired_count = 0
+    total_files = len(file_modification_dates)
 
-	print(s3_file_keys)
-	# Iterated through the list of file keys and retrieve the last modified date
-	for file_key in s3_file_keys:
-		try:
-			response = s3_client.head_object(Bucket=bucket_name, Key=file_key)
-			last_modified = response['LastModified']
-			file_modification_dates[file_key] = last_modified
-		except Exception as e:
-			print(f"Error retrieving last modified date for {file_key}: {e}")
+    for file_key, last_modified in file_modification_dates.items():
+        if current_time - last_modified > timedelta(hours=3):
+            expired_count += 1
 
-	# Check if any files are older than 3 hours
-	current_time = datetime.now()
-	expired = False
-	for file_key, last_modified in file_modification_dates.items():
-		if current_time - last_modified > timedelta(hours=3):
-			expired = True
-			break
+    # Determine the status based on the expiration
+    # Check if less than 80% of the files are expired
+    expiry_threshold = 0.8  # 80%
+    expired = (expired_count / total_files) < expiry_threshold
+    status = "expired" if expired else "success"
 
-	# Determine the status based on the expiration condition
-	status = "expired" if expired else "success"
+    # Create a dict to represent the result including status
+    result = {"status": status, "file_modification_dates": file_modification_dates}
 
-	# Create a dictionary to represent the result including status
-	result = {"status": status, "file_modification_dates": file_modification_dates}
+    # Write the result as JSON to file
+    with open("latest-updated.json", "w") as json_file:
+        json.dump(result, json_file)
 
-	# Write the result as JSON to a local file
-	with open("latest-updated.json", "w") as json_file:
-		json.dump(result, json_file)
-
-	# Upload the local JSON file to S3
-	upload_file("latest-updated.json", bucket_name, "latest-updated.json")
-
+    # Upload the local JSON file to S3
+    upload_file("latest-updated.json", config.pub_s3_bucket, "latest-updated.json")
 
 if __name__ == "__main__":
     main()
