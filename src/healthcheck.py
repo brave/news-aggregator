ad47efc0e4147db4038a038cd0bc72d95a74b919
Nicholas Porter <nporter@brave.com>
2023-10-05 10:05:13 -0700

    Refactor data written to json file

--- src/healthcheck.py
+++ src/healthcheck.py
@@ -1,26 +1,20 @@
-import os
 import json
 
 from config import get_config
 from utils import upload_file, s3_client
 from datetime import datetime, timedelta
 
+config = get_config()
+
 def main():    
-    config = get_config()
     bucket_name = config.pub_s3_bucket
-    sources_dir = config.sources_dir
+    sources_files = config.sources_dir.glob("sources.*_*.csv")
 
     # Initialize an empty dict to store the results
     file_modification_dates = {}
-
-    s3_file_keys = []
-    # Update the s3_file_keys array by iterating through the sources dir
-    for filename in os.listdir(sources_dir):
-        if filename.startswith("sources.") and filename.endswith(".csv"):
-            s3_file_keys.append(os.path.join(sources_dir, filename))
-
+	
     # Iterate through the list of file keys and retrieve the last modified date
-    for file_key in s3_file_keys:
+    for file_key in sources_files:
         try:
             response = s3_client.head_object(Bucket=bucket_name, Key=file_key)
             last_modified = response['LastModified']
@@ -28,14 +22,18 @@ def main():
         except Exception as e:
             print(f"Error retrieving last modified date for {file_key}: {e}")
 
-    # Check if any files are older than 3 hours
     current_time = datetime.now()
     expired_count = 0
     total_files = len(file_modification_dates)
+    json_content = {}
 
+    # Check if any files are older than 3 hours
     for file_key, last_modified in file_modification_dates.items():
-        if current_time - last_modified > timedelta(hours=3):
-            expired_count += 1
+      time_difference = current_time - last_modified
+      is_file_expired = time_difference > timedelta(hours=3)
+      json_content[file_key] = { expired: is_file_expired }
+      if is_file_expired:
+        expired_count += 1
 
     # Determine the status based on the expiration
     # Check if less than 80% of the files are expired
@@ -44,7 +42,7 @@ def main():
     status = "expired" if expired else "success"
 
     # Create a dict to represent the result including status
-    result = {"status": status, "file_modification_dates": file_modification_dates}
+    result = {"status": status, "files": json_content}
 
     # Write the result as JSON to file
     with open("latest-updated.json", "w") as json_file:
