c0cfac4f829188052b1cfd5554e7a8a4c3d3816d
Rana Aurangzaib <aurangzaib048@gmail.com>
2024-04-23 20:39:16 +0500

    Enable DB globally

--- bin/start.sh
+++ bin/start.sh
@@ -37,11 +37,11 @@ if [[ "$task" = "run-all" ]]; then
   if [[ "$SOURCES_FILE" = "sources.en_US" ]]; then
     echo "Generating sources.global.json"
     python -u src/csv_to_global_json.py
-    echo "Apply DB migrations"
-    alembic upgrade head
-    echo "Inserting publisher in DB"
-    python -u src/db_crud.py
   fi
+  echo "Apply DB migrations"
+  alembic upgrade head
+  echo "Inserting publisher in DB"
+  python -u src/db_crud.py
 
   echo "Starting main script..."
   mkdir -p output/feed/cache
